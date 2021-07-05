4f4c353e50312630fedf553c13bc050fc0976ff7
rnrnvv@softbank.ne.jp <ta-sakurai@kddi-tech.com>
2021-07-05 17:40:58 +0900

    Lesson4 5.3

---
+++ app/src/main/java/jp/techacademy/takumi/sakurai/intent/MainActivity.kt
@@ -0,0 +1,24 @@
+package jp.techacademy.takumi.sakurai.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("VALUE1", 10)
+        intent.putExtra("VALUE2", 20)
+        startActivity(intent)
+    }
+}
